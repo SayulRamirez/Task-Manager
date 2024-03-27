3219f80f0f65e4d32ece29a97d54f18ee3fa2c46
Sayul <sayulramirez@gmail.com>
2024-03-27 09:32:52 -0600

    Agregado el método para consultar en la base de datos la tarea donde coincida su id con el id del autor, si no encuentra alguna coincidencia devolvera null o vacío

--- src/main/java/com/task_manager/repositories/TaskRepository.java
+++ src/main/java/com/task_manager/repositories/TaskRepository.java
@@ -2,8 +2,17 @@ package com.task_manager.repositories;
 
 import com.task_manager.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+
+    @Query("""
+    select t from TaskEntity t where t.id =:idTask and t.author.id =:idAuthor
+    """)
+    TaskEntity findTaskByIdAndIdAuthor(Long idTask, Long idAuthor);
+
 }
