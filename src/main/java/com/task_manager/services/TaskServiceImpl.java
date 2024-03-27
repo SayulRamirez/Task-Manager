7cb68bb52bbb87a60d6329ac54aa4acff1d3d1fd
Sayul <sayulramirez@gmail.com>
2024-03-27 00:12:53 -0600

    Agregados la interfaz y la clase para el servicio de las tareas

--- src/main/java/com/task_manager/services/TaskServiceImpl.java
+++ src/main/java/com/task_manager/services/TaskServiceImpl.java
@@ -1,4 +1,52 @@
 package com.task_manager.services;
 
-public class TaskServiceImpl {
+import com.task_manager.domain.Author;
+import com.task_manager.domain.NewTask;
+import com.task_manager.domain.Task;
+import com.task_manager.entities.AuthorEntity;
+import com.task_manager.entities.TaskEntity;
+import com.task_manager.exceptions.AuthorNotFound;
+import com.task_manager.repositories.AuthorRepository;
+import com.task_manager.repositories.TaskRepository;
+import com.task_manager.services.interfaces.TaskService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final AuthorRepository authorRepository;
+    private final TaskRepository taskRepository;
+
+    public TaskServiceImpl(AuthorRepository authorRepository, TaskRepository taskRepository) {
+        this.authorRepository = authorRepository;
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public Task createTask(NewTask newTask) {
+
+        AuthorEntity authorEntity = authorRepository.findById(newTask.id_author()).orElse(null);
+
+        if (authorEntity == null) throw new AuthorNotFound("Author not found whit id " + newTask.id_author());
+
+        TaskEntity taskEntity = new TaskEntity(
+                null,
+                newTask.title(),
+                newTask.description(),
+                null,
+                newTask.start_date(),
+                authorEntity
+        );
+
+        taskRepository.save(taskEntity);
+
+        return new Task(
+                taskEntity.getId(),
+                taskEntity.getTitle(),
+                taskEntity.getDescription(),
+                taskEntity.getStatus(),
+                taskEntity.getStartDate(),
+                new Author(taskEntity.getAuthor().getId(), taskEntity.getAuthor().getNick())
+        );
+    }
 }
--- src/main/java/com/task_manager/services/interfaces/TaskService.java
+++ src/main/java/com/task_manager/services/interfaces/TaskService.java
@@ -1,4 +1,14 @@
 package com.task_manager.services.interfaces;
 
+import com.task_manager.domain.NewTask;
+import com.task_manager.domain.Task;
+
 public interface TaskService {
+
+    /**
+     * Create a new task
+     * @param newTask {@link NewTask}
+     * @return Task
+     */
+    Task createTask(NewTask newTask);
 }
