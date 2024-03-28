5df547d91eeea11b62e67f9b5dfff25a76a522f0
Sayul <sayulramirez@gmail.com>
2024-03-28 00:30:26 -0600

    Agregado un método para verificar si existe el email en la base de datos

---
+++ src/main/java/com/task_manager/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.task_manager.repositories;
+
+import com.task_manager.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    boolean existsByEmail(String email);
+}
