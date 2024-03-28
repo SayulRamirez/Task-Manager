cb38009b2279bb1bdf4c355b95af7232319fedff
Sayul <sayulramirez@gmail.com>
2024-03-28 00:29:11 -0600

    Agregado un método para verificar si existe un nick en la base de datos

--- src/main/java/com/task_manager/repositories/AuthorRepository.java
+++ src/main/java/com/task_manager/repositories/AuthorRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AuthorRepository extends JpaRepository<AuthorEntity, Long> {
 
+    boolean existsByNick(String nick);
 }
