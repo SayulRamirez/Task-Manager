4e9c8e6c9ab0a161e6fbc8d11af014b54d29f0d1
Sayul <sayulramirez@gmail.com>
2024-03-29 11:13:59 -0600

    Creado el servicio que se encargara de generar el token

---
+++ src/main/java/com/task_manager/security/JwtService.java
@@ -0,0 +1,11 @@
+package com.task_manager.security;
+
+import com.task_manager.entities.UserEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String getToken(UserEntity user) {
+        return null;
+    }
+}
