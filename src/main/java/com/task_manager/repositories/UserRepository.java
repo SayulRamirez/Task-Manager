5441f370ebca4591ea8d377548e0c3b955d6f3af
Sayul <sayulramirez@gmail.com>
2024-03-29 11:04:05 -0600

    modificado el método existsByEmail por existsByUsername y agregado el método findByUsername

--- src/main/java/com/task_manager/repositories/UserRepository.java
+++ src/main/java/com/task_manager/repositories/UserRepository.java
@@ -4,8 +4,12 @@ import com.task_manager.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    boolean existsByEmail(String email);
+    boolean existsByUsername(String email);
+
+    Optional<UserEntity> findByUsername(String email);
 }
