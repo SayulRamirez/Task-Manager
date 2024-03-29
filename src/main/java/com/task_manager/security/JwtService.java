f4cc1dbe0beac300a190bdc8ffc424303aad50c3
Sayul <sayulramirez@gmail.com>
2024-03-29 11:32:04 -0600

    Implementado el método para la generación del token

--- src/main/java/com/task_manager/security/JwtService.java
+++ src/main/java/com/task_manager/security/JwtService.java
@@ -1,11 +1,43 @@
 package com.task_manager.security;
 
-import com.task_manager.entities.UserEntity;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.DefaultMessageCodesResolver;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
-    public String getToken(UserEntity user) {
-        return null;
+
+    private final String SECRET_KEY = "BI3HB253H3H5BI343Y5AT43HI3HE54BM95H535H5IBI345D3I5BIB4I23HI53H3HF4I5634F54LL75J7GO3587B973497B";
+    public String getToken(UserDetails user) {
+
+        return getToken(new HashMap<>(), user);
+    }
+
+    private String getToken(Map<String, Object> extraClaims, UserDetails user) {
+
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(user.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getKey() {
+
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
