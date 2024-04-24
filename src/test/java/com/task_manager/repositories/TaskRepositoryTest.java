472c1f5017e238ba51aa72ed2f1f624a88fb24a5
Sayul <sayulramirez@gmail.com>
2024-04-24 09:08:57 -0600

    Creando la clase de pruebas para TaskRepository, se agrego el método beforeEach y el primer test para la creación de las tareas

---
+++ src/test/java/com/task_manager/repositories/TaskRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.task_manager.repositories;
+
+import com.task_manager.entities.AuthorEntity;
+import com.task_manager.entities.TaskEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+public class TaskRepositoryTest {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
+    private TaskEntity taskEntity;
+
+    @BeforeEach
+    void setup() {
+
+        AuthorEntity authorEntity = new AuthorEntity(null, "juan");
+
+        authorRepository.save(authorEntity);
+
+        taskEntity = new TaskEntity(null,
+                "Tarea de prueba",
+                "Sobre esta tarea se realizarán todos los casos de prueba",
+                null,
+                LocalDateTime.of(2024, Month.DECEMBER, 20, 9, 12),
+                authorEntity);
+    }
+
+    @Test
+    void createTask() {
+
+        TaskEntity task = taskRepository.save(taskEntity);
+
+        assertThat(task).isNotNull();
+        assertThat(task.getId()).isNotZero();
+    }
+}
