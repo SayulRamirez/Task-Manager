729801b1a20ac11445d51d76399a33c2bf504867
Sayul <sayulramirez@gmail.com>
2024-04-23 23:21:27 -0600

    Corrigiendo el nombre de la clase y agregando los test cases para buscar el author por su id

--- src/test/java/com/task_manager/repositories/AuthorRepositoryTest.java
+++ src/test/java/com/task_manager/repositories/AuthorRepositoryTest.java
@@ -1,30 +1,61 @@
 package com.task_manager.repositories;
 
 import com.task_manager.entities.AuthorEntity;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
-public class AuthorRespositoryTest {
+public class AuthorRepositoryTest {
 
     @Autowired
     private AuthorRepository authorRepository;
+    private AuthorEntity authorEntity;
+
+    @BeforeEach
+    void setup() {
+        authorEntity = new AuthorEntity(null, "juan");
+    }
 
     @Test
     void createAuthor() {
-
-        AuthorEntity authorEntity = new AuthorEntity(null, "juan");
 
         AuthorEntity author = authorRepository.save(authorEntity);
 
         assertThat(author).isNotNull();
         assertThat(author.getId()).isNotZero();
+    }
+
+    @Test
+    void findAuthorById() {
+
+        AuthorEntity authorSave = authorRepository.save(authorEntity);
+
+        Optional<AuthorEntity> author = authorRepository.findById(authorSave.getId());
+
+        assertThat(author).isNotNull();
+        assertThat(author.isPresent()).isTrue();
+
+        String nick = author.get().getNick();
+
+        assertThat(nick).isEqualTo("juan");
+
+    }
+
+    @Test
+    void notFoundAuthorById() {
+
+        Optional<AuthorEntity> author = authorRepository.findById(2L);
+
+        assertThat(author.isEmpty()).isTrue();
     }
 }
